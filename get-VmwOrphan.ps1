3cbbd9e54b559e291f161a20a8e4b264cda580db
chrisfederico1 <chris.federico@yahoo.com>
2021-11-19 10:43:52 -0600

    First Crack at code

--- get-VmwOrphan.ps1
+++ get-VmwOrphan.ps1
@@ -23,7 +23,10 @@ function Get-VmwOrphan{
         [parameter(Mandatory=$true,ValueFromPipeline=$true)]
         [PSObject[]]$Datastore
       )
-      
+     
+      #Line 30-62: The script constructs the SearchDatastoreSubFolders parameter in the Begin block.
+      #This parameter will be the same for all datastores.
+
       Begin{
         $flags = New-Object VMware.Vim.FileQueryFlags
         $flags.FileOwner = $true
@@ -53,10 +56,14 @@ function Get-VmwOrphan{
         
         $searchSpec = New-Object VMware.Vim.HostDatastoreBrowserSearchSpec
         $searchSpec.details = $flags
+        #Line 61: To get maximum detail for the returned files, 
+        #the script uses all available FileQuery variation
         $searchSpec.Query = $qFloppy,$qFolder,$qISO,$qConfig,$qTemplate,$qDisk,$qLog,$qRAM,$qSnap
         $searchSpec.sortFoldersFirst = $true
       }
+
       
+     #Line 69-71: Cheap OBN implementation 
       Process{
         foreach($ds in $Datastore){
           if($ds.GetType().Name -eq "String"){
@@ -64,13 +71,22 @@ function Get-VmwOrphan{
           }
      
     # Only shared VMFS datastore
+    #Line 75: The script only looks at shared VMFS datastores
           if($ds.Type -eq "VMFS" -and $ds.ExtensionData.Summary.MultipleHostAccess -and $ds.State -eq 'Available'){
             Write-Verbose -Message "$(Get-Date)`t$((Get-PSCallStack)[0].Command)`tLooking at $($ds.Name)"
       
+    
+    #Line 84: This hash table will be used to determine which file/folder is orphaned or not.
+    # First all the files the method fins are placed in the hash table.
+    # Then the files that belong to VMs and Templates are removed.
+    # Finally some system files are removed. What is left are orphaned files.
     # Define file DB
             $fileTab = @{}
      
     # Get datastore files
+    #Line 92: Get all the files on the datastore with the SearchDatastoreSubFolders method
+    #Line 90-103: Enter the files in the hash table
+
             $dsBrowser = Get-View -Id $ds.ExtensionData.browser
             $rootPath = "[" + $ds.Name + "]"
             $searchResult = $dsBrowser.SearchDatastoreSubFolders($rootPath, $searchSpec) | Sort-Object -Property {$_.FolderPath.Length}
@@ -79,6 +95,8 @@ function Get-VmwOrphan{
                 $key = "$($folder.FolderPath)$(if($folder.FolderPath[-1] -eq ']'){' '})$($file.Path)"
                 $fileTab.Add($key,$file)
      
+                #Line 100-102: Take care of the folder entries. If a file inside a folder is encountered, 
+                #the script also removes the folder itself from the hash table
                 $folderKey = "$($folder.FolderPath.TrimEnd('/'))"
                 if($fileTab.ContainsKey($folderKey)){
                   $fileTab.Remove($folderKey)
@@ -87,6 +105,8 @@ function Get-VmwOrphan{
             }
       
     # Get VM inventory
+    # Line 110-113: Remove all files that belong to registered VMs from the hash table
+
             Get-VM -Datastore $ds | %{
               $_.ExtensionData.LayoutEx.File | %{
                 if($fileTab.ContainsKey($_.Name)){
@@ -95,6 +115,7 @@ function Get-VmwOrphan{
               }
             }
       
+    # Line 120-123: Remove all files that belong to registered Templates from the hash table
     # Get Template inventory
             Get-Template | where {$_.DatastoreIdList -contains $ds.Id} | %{
               $_.ExtensionData.LayoutEx.File | %{
@@ -103,13 +124,13 @@ function Get-VmwOrphan{
                 }
               }
             }
-     
+     #Line 129-131: Remove system files. For now these all files that are located in folders that start with a dot, or in a folder named vmkdump.
     # Remove system files & folders from list
             $systemFiles = $fileTab.Keys | where{$_ -match "] \.|vmkdump"}
             $systemFiles | %{
               $fileTab.Remove($_)
             }
-     
+     #Line 135-159: If there are entries left in the hash table, collect more information and create an ordered object containing that information.
     # Organise remaining files
             if($fileTab.Count){
               $fileTab.GetEnumerator() | %{
